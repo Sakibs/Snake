ce807376a6f0071ad214b99ee82d587cf539010e
Sakib Shaikh <skb_shak@Sakib-Shaikhs-MacBook-Air.local>
2013-03-21 23:21:43 -0700

    v1.3 complete, renamed compilation output, major internal overhauls

--- README.md
+++ README.md
@@ -3,6 +3,23 @@ My adaptation of the popular snake game made the ncurses library in C for the te
 
 Version Log
 -----------
+v1.3
+External
+- implemented food
+- snakey grows when eating food
+- beep sounds when eating food
+- cursor is now hidden
+- snakey can no longer turn around and eat his own neck
+Internal
+- snakey no longer an array, he is now represented by a struct with a length variable and an array of parts inside the struct.
+- efficient functions to handle menu display
+Bugs
+- when eating the first bite of food, there is a delay in the beep
+- when resetting, the food for some reason is not reset even though the set_food_coord function is called!
+Todo
+- add score system
+- better menu interface
+
 v1.2
 - set head of snake to index 0
 - added collision detection for head to body
--- main.c
+++ main.c
@@ -1,38 +1,48 @@
 #include <ncurses.h>
 
-#define TICKRATE 130
+#define TICKRATE 150
 
 #define WORLD_WIDTH 50
 #define WORLD_HEIGHT 20
 
-#define SNAKEY_LENGTH 40
-#define SNAKEY_MIN_LENGTH 4
+#define SNAKEY_MAX_LENGTH 40
+#define SNAKEY_MIN_LENGTH 5
 
 enum direction {UP, DOWN, RIGHT, LEFT};
 enum gamestate {PLAY, PAUSE, LOST};
 
 int game_state;
 
+typedef struct coord{
+	int x;
+	int y;
+} Food;
+
 typedef struct spart{
 	int x;
 	int y;
 } snakeypart;
 
-struct Snake{
+typedef struct SnakeData {
 	int cur_snakey_length;
-	snakeypart body[SNAKEY_LENGTH];
-};
+	snakeypart body[SNAKEY_MAX_LENGTH];
+} Snake;
 
-int check_collision(snakeypart snakey[]);
-int setup_snakey(snakeypart snakey[], int begx, int begy);
-int update_snakey_pos(snakeypart snakey[], int direction);
-int move_snakey(WINDOW *win, int direction, snakeypart snakey[]);
+int check_collision(Snake *snakey);
+int setup_snakey(Snake* snakey, int begx, int begy);
+int update_snakey_pos(Snake *snakey, int direction);
+int grow_snakey(Snake *snakey);
+int update_screen(WINDOW *win, Snake *snakey, int direction, Food *byte);
+int update_menu(WINDOW* menu, int message);
 
+int set_food_coord(Snake *Snakey, Food *byte);
+int check_ate_food(Snake *snakey, Food *byte);
 
 int main()
 {
     WINDOW *snakey_world;
 	WINDOW *menu_win;
+	//hide the cursor, we don't want to see it	
     int offsetx, offsety, menu_offsety, i, ch;
 
     initscr();
@@ -40,55 +50,64 @@ int main()
 	cbreak();
 	timeout(TICKRATE);
 	keypad(stdscr, true);
+	curs_set(0); //hide the cursor
+
 	
 	//printw("Snake v 1.1 - Press p to play/pause, x to quit\n");
     refresh();
 
     offsetx = (COLS - WORLD_WIDTH)/2;
     offsety = (LINES - WORLD_HEIGHT)/2;
-	menu_offsety = (offsety - 5);
+	menu_offsety = (offsety - 4);
 
     snakey_world = newwin(WORLD_HEIGHT, WORLD_WIDTH, offsety, offsetx);
 
 	//draw the menu window
-	menu_win = newwin(5, WORLD_WIDTH, menu_offsety, offsetx);
-	wprintw(menu_win, "Snake v 1.1 - Press p to play/pause, x to quit\n");
-	wrefresh(menu_win);
+	menu_win = newwin(4, WORLD_WIDTH, menu_offsety, offsetx);
+	update_menu(menu_win, 0);
+
+	//Declare and setup snake and food
+	Snake snakey;
+	Food byte;
 	
-	//snakey that is displayed on the screen is displayed here.
-	snakeypart snakey[SNAKEY_LENGTH];
-	
-	int sbegx = WORLD_WIDTH - (WORLD_WIDTH - SNAKEY_LENGTH)/2 -1;
+	int sbegx = WORLD_WIDTH - (WORLD_WIDTH - SNAKEY_MIN_LENGTH)/2 -1;
 	int sbegy = (WORLD_HEIGHT - 1)/2;
+	setup_snakey(&snakey, sbegx, sbegy);
+	set_food_coord(&snakey, &byte);
 	
-	setup_snakey(snakey, sbegx, sbegy);
-	
+	//setup game variables
 	int cur_dir = RIGHT;
 	game_state = PLAY;
 	
 	while((ch = getch()) != 'x'){
-		move_snakey(snakey_world, cur_dir, snakey);
+		update_screen(snakey_world, &snakey, cur_dir, &byte);
 		switch(game_state){
 			case PLAY:
 				if(ch != ERR){
 					switch(ch){
+						//if were moving make sure we dont move back to collide with the
+						//part right behind the head
 						case KEY_UP:
-							cur_dir = UP;
+							if(cur_dir != DOWN)
+								cur_dir = UP;
 							break;
 						case KEY_DOWN:
-							cur_dir = DOWN;
+							if(cur_dir != UP)
+								cur_dir = DOWN;
 							break;
 						case KEY_RIGHT:
-							cur_dir = RIGHT;
+							if(cur_dir != LEFT)
+								cur_dir = RIGHT;
 							break;
 						case KEY_LEFT:
-							cur_dir = LEFT;
+							if(cur_dir != RIGHT)
+								cur_dir = LEFT;
 							break;
 						case 'p':
 							game_state = PAUSE;
 							break;
 						case 'r':
-							setup_snakey(snakey, sbegx, sbegy);
+							setup_snakey(&snakey, sbegx, sbegy);
 							cur_dir = RIGHT;
 							break;
 						default:
@@ -103,7 +122,8 @@ int main()
 							game_state = PLAY;
 							break;
 						case 'r':
-							setup_snakey(snakey, sbegx, sbegy);
+							setup_snakey(&snakey, sbegx, sbegy);
+							set_food_coord(&snakey, &byte);
 							game_state = PLAY;
 							cur_dir = RIGHT;
 							break;
@@ -113,18 +133,14 @@ int main()
 				}
 				break;
 			case LOST:
-				wclear(menu_win);
-				wprintw(menu_win, "Game Over...\nPress x to quit, r to reset.\n");
-				wrefresh(menu_win);
+				update_menu(menu_win, 1);
 				
 				if(ch != ERR){
 					switch (ch) {
 						case 'r': //reset snakey and then reset the screen
-							wclear(menu_win);
-							wprintw(menu_win, "Snake v 1.1 - Press p to play/pause\nx to quit\nr to reset");
-							wrefresh(menu_win);
-							
-							setup_snakey(snakey, sbegx, sbegy);
+							update_menu(menu_win, 0);
+							setup_snakey(&snakey, sbegx, sbegy);
+							set_food_coord(&snakey, &byte);
 							game_state = PLAY;
 							cur_dir = RIGHT;
 							break;
@@ -135,103 +151,181 @@ int main()
 				break;
 			default: 
 				break;
-
 		}
-		//move_snakey(snakey_world, cur_dir, snakey);
-
 	}
 	
     delwin(snakey_world);
-
     endwin();
-
     return 0;
 }
 
 
-int setup_snakey(snakeypart snakey[], int begx, int begy)
-{
+int setup_snakey(Snake *snakey, int begx, int begy){
+	snakey->cur_snakey_length = SNAKEY_MIN_LENGTH;
 	int i;
-	for (i = 0; i<SNAKEY_LENGTH; i++){
-		snakey[i].x = begx-i;
-		snakey[i].y = begy;
-	}
 	
+	//set up initial snakey body positions
+	for (i = 0; i<SNAKEY_MIN_LENGTH; i++){
+		snakey->body[i].x = begx-i;
+		snakey->body[i].y = begy;
+	}
+	//set the rest of the body values to 0 so no junk values remain
+	for(; i<SNAKEY_MAX_LENGTH; i++){
+		snakey->body[i].x = 0;
+		snakey->body[i].y = 0;
+	}
 	return 0;
 }
 
-int move_snakey(WINDOW *win, int direction, snakeypart snakey[]){
+int update_screen(WINDOW *win, Snake *snakey, int direction, Food *byte){
 	wclear(win);
 	
 	int i;
+	int cur_snakey_len = snakey->cur_snakey_length;
 	
 	if(game_state == PLAY)
-	{
 		update_snakey_pos(snakey, direction);
-	}
 	
-	for(i=1; i<SNAKEY_LENGTH; i++) {
-		mvwaddch(win, snakey[i].y, snakey[i].x, '#');
-	}
+	for(i=1; i<cur_snakey_len; i++)
+		mvwaddch(win, snakey->body[i].y, snakey->body[i].x, '#');
 	
+	int collided = 0;
+	//check if snake ate food
+	if(check_ate_food(snakey, byte) == 1){
+		mvwaddch(win, snakey->body[0].y, snakey->body[0].x, 'O');
+		set_food_coord(snakey, byte);
+		grow_snakey(snakey);
+		beep();
+		collided = 1;
+	}
+	else{
+		mvwaddch(win, byte->y, byte->x, '@');
+	}	
 	//check if player caused snakey to collide, update game_state if the head
 	//of the snake intersects with any other parts of the snake.
-	if(check_collision(snakey) == 1)
-	{
-		mvwaddch(win, snakey[0].y, snakey[0].x, 'X');
+	if(check_collision(snakey) == 1){
+		mvwaddch(win, snakey->body[0].y, snakey->body[0].x, 'X');
 		game_state = LOST;
+		collided = 1;
 		//flash();
 	}
-	else
-		mvwaddch(win, snakey[0].y, snakey[0].x, '#');
+	
+	if(!collided)
+		mvwaddch(win, snakey->body[0].y, snakey->body[0].x, '#');
 
-	box(win, 0, 0);
-	
+	box(win, 0, 0);	
 	wrefresh(win);
-	
 	return 0;
 }
 
-int update_snakey_pos(snakeypart snakey[], int direction)
-{
+int update_snakey_pos(Snake *snakey, int direction){
 	int i;
-	for(i=SNAKEY_LENGTH-1; i>0 ; i--){
-		snakey[i] = snakey[i-1];
+	int cur_snakey_len = snakey->cur_snakey_length;
+	
+	for(i=cur_snakey_len-1; i>0 ; i--){
+		snakey->body[i] = snakey->body[i-1];
 	}
 	
-	int x = snakey[0].x;
-	int y = snakey[0].y;
-	
+	int x = snakey->body[0].x;
+	int y = snakey->body[0].y;
+	//check the direction of the snake and update coordinates of head accordingly
+	//wrap around if at border
 	switch(direction){
 		case UP:
-			y-1 == 0 ? y = WORLD_HEIGHT - 2 : y--;
+			y-1 == 0 ? y = WORLD_HEIGHT-2 : y--;
 			break;
 		case DOWN:
-			y+1 == WORLD_HEIGHT - 1 ? y = 1 : y++;
+			y+1 == WORLD_HEIGHT-1 ? y = 1 : y++;
 			break;
 		case RIGHT:
-			x+1 == WORLD_WIDTH - 1 ? x = 1 : x++;
+			x+1 == WORLD_WIDTH-1 ? x = 1 : x++;
 			break;
 		case LEFT: 
-			x-1 == 0 ? x = WORLD_WIDTH - 2 : x--;
+			x-1 == 0 ? x = WORLD_WIDTH-2 : x--;
 			break;
 		default: 
 			break;
 	}
-	
-	snakey[0].x = x;
-	snakey[0].y = y;
+	snakey->body[0].x = x;
+	snakey->body[0].y = y;
+	return 0;
 }
 
-int check_collision(snakeypart snakey[])
-{
-	int headx = snakey[0].x;
-	int heady = snakey[0].y;
+int grow_snakey(Snake* snakey){
+	if(snakey->cur_snakey_length < SNAKEY_MAX_LENGTH){
+		snakey->cur_snakey_length++;
+		
+		//make the coordinates of the new part the coordinates of the last part in snakey
+		int new_part = snakey->cur_snakey_length;
+		snakey->body[new_part] = snakey->body[new_part-1];
+	}
+}
+
+int check_collision(Snake *snakey){
+	int headx = snakey->body[0].x;
+	int heady = snakey->body[0].y;
 	
+	int cur_len = snakey->cur_snakey_length;
+	//make sure head of snakey does not intersect with any othe part of the body
 	int i;
-	for(i = 1; i<SNAKEY_LENGTH; i++)
-		if(snakey[i].x == headx && snakey[i].y == heady)
+	for(i = 1; i<cur_len; i++)
+		if(snakey->body[i].x == headx && snakey->body[i].y == heady)
 			return 1;
 	
 	return 0;
+}
+
+int check_ate_food(Snake *snakey, Food *byte){
+	if(snakey->body[0].x == byte->x && snakey->body[0].y == byte->y)
+		return 1;
+	return 0;
+}
+
+int update_menu(WINDOW *menu, int message){
+	wclear(menu);
+	switch(message){
+		case 1: //the game over screen
+			wprintw(menu, "Game Over...\nPress x to quit, r to reset.\n");
+			break;
+		default:
+			wprintw(menu, "Snake v 1.3\nPress p to play/pause, x to quit, r to reset");
+			break;
+	}
+	wrefresh(menu);
+	return 0;
+}
+
+int set_food_coord(Snake *snakey, Food* byte){
+	int clear = 1;
+	int newx, newy;
+	/*	generate new coord for food, if it is intersecting with the coord of one of the 
+		snake body parts, keep trying until we get a valid coordinate*/
+	do{
+		//reset clear if it was set to 0 on last iteration
+		if(clear == 0)
+			clear = 1;
+			
+		newx = rand() % WORLD_WIDTH;
+		newy = rand() % WORLD_HEIGHT;
+		//check if newx or newy are on the border of the box, if they are
+		//just increment them or decrement them.
+		if(newx == 0)
+			newx++;
+		else if(newx == 49)
+			newx--;
+		if(newy == 0)
+			newy++;
+		else if(newy == 49)
+			newy--;
+		
+		int snakey_len = snakey->cur_snakey_length;
+		int i;
+		
+		for(i=0; i<snakey_len; i++)
+			if(snakey->body[i].x == newx && snakey->body[i].y == newy)
+				clear = 0;		
+	}while(clear != 1);
+	
+	byte->x = newx;
+	byte->y = newy;
 }
